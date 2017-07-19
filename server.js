a172364e5830e6c25712bb0ec7dd24f6661b8138
Eric Sowell <eric.sowell@gmail.com>
2017-07-19 09:07:22 -0500

    fixing things by adding cors back

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ let app = express();
 
 app.use(express.static('public'));
 
-// var allowCrossDomain = function(req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//     res.header('Access-Control-Allow-Headers', '*');
-//
-//     next();
-// }
-//
-// app.use(allowCrossDomain);
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', '*');
+
+    next();
+}
+
+app.use(allowCrossDomain);
 
 /*
 GET /api/dragonlance
